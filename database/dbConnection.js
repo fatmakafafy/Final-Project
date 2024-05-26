a1e72047e660ec0f9021a8912f7b9977b7954119
fatmakafafy <fatmamostafakafafy@gmail.com>
2024-05-26 22:51:32 +0300

    change2

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -10,8 +10,12 @@
 // }  
 import mongoose from "mongoose";
 export function dbConnection() {
-    mongoose.connect(process.env.DB_CONNECTION, {
+    mongoose.connect('mongodb+srv://fatmamostafakafafy:q2Cp4m5Vu4Xa3TWw@cluster0.rhp0umi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    })
+    }).then(() => {
+                console.log("Database Connected")
+            }).catch((err) => {
+                console.log("Error", err)
+            })
 };
