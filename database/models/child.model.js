a21735f26aad1f1363b64898a40953e7d1673fe2
fatmakafafy <fatmamostafakafafy@gmail.com>
2024-06-22 03:15:54 +0300

    change29

--- database/models/child.model.js
+++ database/models/child.model.js
@@ -37,7 +37,7 @@ const childSchema = new mongoose.Schema({
     },
     parentId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'user',
         required: true
     },
     createdAt: {
